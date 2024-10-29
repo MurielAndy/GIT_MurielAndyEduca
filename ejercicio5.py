bb5c808fa74d25979048362153c5d20d0f1bb806
MurielAndy <amurielcab@educacion.navarra.es>
2024-10-29 23:10:38 +0100

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -3,4 +3,3 @@ horas_trabajadas = float(input("Ingrese el numero de horas trabajadas: "))
 coste_por_hora = float(input("Ingrese el coste por hora" ))
 pago_total = horas_trabajadas * coste_por_hora
 print ("El pago total es",pago_total)
- 
