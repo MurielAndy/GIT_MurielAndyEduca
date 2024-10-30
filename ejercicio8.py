d470baec2321d1d7441256624b51398772b669a5
MurielAndy <amurielcab@educacion.navarra.es>
2024-10-30 17:02:43 +0100

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -4,6 +4,5 @@ m = int(input("Introduce el segundo número entero (m): "))
 # Calcular el cociente y el resto
 c = n // m  # Cociente
 r = n % m   # Resto
-
 # Mostrar el resultado al usuario
 print(f"La {n} entre {m} da un cociente {c} y un resto {r}.")
